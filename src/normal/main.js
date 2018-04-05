489ceb1dbdc52276cdb90c26e9a3c1362eb376b1
murata <murata@kmc.gr.jp>
2018-04-06 01:52:08 +0900

    build

---
+++ src/normal/main.js
@@ -0,0 +1,8 @@
+// 'use strict';
+window.$ = window.jQuery = require('jquery');
+import Vue from 'vue/dist/vue.esm.js';
+console.log(Vue);
+// import memoMemo from '../vue/memomemo.vue';
+// import MemoStore from '../js/memostore';
+// memoMemo.store = MemoStore;
+// new Vue(memoMemo).$mount('#app');
