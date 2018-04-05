1e5a1f844a22efc78ae48eb4d0ec766fffbf8a10
murata <murata@kmc.gr.jp>
2018-04-06 05:04:58 +0900

    created tovue

--- src/class/store.js
+++ src/class/store.js
@@ -25,34 +25,3 @@ export default toVuex(class Store {
   // 代入用外部公開mutationを自動で生成
   set $$storeStateA(_) {}
 });
-
-/*
-export default new Vuex.Store({
-  state: {
-    storeStateA: 'STORE_STATE_A',
-  },
-  getters: {
-    storeGetterFunc: (state, getters) => arg => {
-      return arg;
-    },
-    storeGetterA: (state, getters) => {
-      return 'STORE_GETTER_A';
-    },
-    storeGetterB: (state, getters) => {
-      return getters.storeGetterFunc('HOGE') + state.storeStateA +
-          getters.storeGetterA + ': STORE_GETTER_B';
-    }
-  },
-  mutations: {
-    add(state) {
-      state.storeStateA = state.storeStateA + 'A';
-    },
-    addArg(state, args) {
-      state.storeStateA = state.storeStateA + args;
-    },
-    $$storeStateA(state, arg) {
-      state.storeStateA = arg;
-    }
-  }
-})
-*/
